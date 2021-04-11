48a57812ad2a22c877515aadbea511d2a5878077
Daniel Omiya <danielomiya@gmail.com>
2021-04-11 00:35:22 -0300

    Include simple test on main

--- app/main.cpp
+++ app/main.cpp
@@ -1,20 +1,24 @@
 #include "nodeClass.hpp"
 #include "treeClass.hpp"
-
 #include <iostream>
-#include <stdlib.h>
+#include "fileReaderClass.hpp"
 
 using namespace std;
 
-int main()
-{
+int main(int argc, char *argv[]) {
     treeClass tree;
+    fileReaderClass reader("data/billboardData.csv");
 
     tree.insert(50);
     tree.insert(40);
     tree.insert(60);
     tree.insert(30);
-    cout << tree.searchNode(60, tree.getRoot())->getValue();
+    cout << tree.searchNode(60, tree.getRoot())->getValue() << endl;
+
+    auto songs = reader.getData();
+    for (auto &song : songs) {
+        cout << song.artist << endl;
+    }
 
     return 0;
 }
